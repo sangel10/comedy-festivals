d87c395a9b4a26e4c5a70b215cb9101c8c9a9913
Santiago Angel <santiagoangel10@gmail.com>
2019-11-20 13:07:59 -0500

    Submission notes, favicon and title

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App-header" >
       <h1>About/FAQ</h1>
       <h2>WTF is this? </h2>
-        <p> If you sign up for this newsletter you'll get a regular email telling you which comedy festivals are accepting applcations.</p>
+        <p> If you sign up for this newsletter you'll get a regular email telling you which comedy festivals are accepting applications.</p>
       <h2>Why the fuck did you make this?</h2>
         <p>
           There's a ton of comedy festivals, nobody knows when the fuck to apply to them.
@@ -22,19 +22,23 @@ function App() {
           Just kidding, every website is a gift from God (who is Muslim btw).
 
           I did my best to find the dates on websites, however it's likely there are errors.
-          If you would like to submit a change you can do so <a href={SUBMISSION_URL} target="_blank" rel="noopener noreferrer">HERE</a>
+          If you would like to submit a change you can do so <a href={SUBMISSION_URL} target="_blank" rel="noopener noreferrer">HERE</a>.
         </p>
 
+      <h2>How do I know this is legit? Where can I see the data</h2>
+        <p>
+          <a href="https://docs.google.com/spreadsheets/d/1f9JYuJCNmhzrikx7Thh4_N_kG-p1k2V_Vq5_m1C4pp4" target="_blank" rel="noopener noreferrer">You can see the data here.</a>
+        </p>
       <h2>How can I add my festival?</h2>
         <p>
           <a href={SUBMISSION_URL} target="_blank" rel="noopener noreferrer">Fill this out </a>
         </p>
       <h2>The info you have for a festival is wrong, how can I change it?</h2>
         <p>
-          Get the festival ID and <a href={SUBMISSION_URL} target="_blank" rel="noopener noreferrer">Fill this out</a>
+          Get the festival ID and <a href={SUBMISSION_URL} target="_blank" rel="noopener noreferrer">Fill this out.</a>
         </p>
       <h2>Contact</h2>
-        <p><a target="_blank" rel="noopener noreferrer" href="mailto:comedyfestivals@gmail.com">E-mail me</a></p>
+        <p><a target="_blank" rel="noopener noreferrer" href="mailto:comedyfestivals@gmail.com">E-mail me</a>.</p>
       <h2>Sign up for the newsletter</h2>
       <div className="mailchimp-container">
         <MailchimpSubscribe url={url} style={{ display: 'flex', alignItems: 'center' }}/>
